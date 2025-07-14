e56cad69356613683c527bf0e8658a952b3dc8b7
Ivan Poliarush <sashapolarush@gmail.com>
2025-07-14 09:42:51 +0300

    feat: add cloud worker endpoint

--- src/index.js
+++ src/index.js
@@ -1,23 +1,56 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+const PATHNAME_MAPS = {
+	api: [
+		/^\/alphacrm\.php$/,
+		/^\/index\.test\.php$/,
+		/^\/proxy\.php$/,
+		/^\/api\//,
+		/^\/ajax\//,
+		/^\/c1\//,
+		/^\/order\//,
+		/^\/sms\//,
+		/^\/software\//,
+		/^\/_work\//,
+		/^\/mailgun\/webhook/,
+	],
+	cabinet: [/^\/account\//, /^\/backend\//, /^\/panel(\/.*)?$/, /^\/panel$/, /^\/[a-z]{2}\/panel(\/.*)?$/],
+};
+
+const HOSTNAME_MAPS = {
+	landing: 'test1.com',
+	api: 'test2.com',
+	cabinet: 'test3.com',
+};
+
+const resolveTargetType = (pathname) => {
+	if (PATHNAME_MAPS.api.some((regex) => regex.test(pathname))) return 'api';
+	if (PATHNAME_MAPS.cabinet.some((regex) => regex.test(pathname))) return 'cabinet';
+	return 'landing';
+};
 
 export default {
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url);
-		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
-			default:
-				return new Response('Not Found', { status: 404 });
-		}
+		const targetType = resolveTargetType(url.pathname);
+		const targetHostname = HOSTNAME_MAPS[targetType];
+
+		url.hostname = targetHostname;
+
+		const modifiedRequest = new Request(url, {
+			method: request.method,
+			headers: request.headers,
+			body: request.method !== 'GET' && request.method !== 'HEAD' ? await request.blob() : undefined,
+		});
+
+		const response = await fetch(modifiedRequest);
+
+		return new Response(response.body, {
+			status: response.status,
+			headers: {
+				...Object.fromEntries(response.headers),
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Credentials': 'true',
+				'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+			},
+		});
 	},
 };
